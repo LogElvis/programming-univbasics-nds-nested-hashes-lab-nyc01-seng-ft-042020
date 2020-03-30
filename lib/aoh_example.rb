fa1e001bb91df85f22dbe76ef388dff86b22f819
Elvis Ning <ningelvis@gmail.com>
2020-03-30 18:28:29 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,7 +25,8 @@ def literal_aoh
     {:name => "Bruce Green", :occupation => "Fan of Mildred"},
     {:name => "Don Gately", :occupation => "Live-in Staff"},
     {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
-    
+    {:name => "Kate Gompert", :occupation => "None"},
+    {:name => "Pat Monteseian", :occupation => "Staff"}
     ]
 end
 
